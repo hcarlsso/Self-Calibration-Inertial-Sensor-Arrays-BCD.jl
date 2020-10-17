6fdc84d4b8577534668bdb5b260b9d4606be1448
Hakan Carlsson <hakan.carlsson@gmail.com>
2020-10-18 01:41:55 +0200

    Increase speed bcd

--- src/BcdV3.jl
+++ src/BcdV3.jl
@@ -4,7 +4,7 @@ using StaticArrays
 using LinearAlgebra
 using Distributed
 using Printf
-
+using Base.Threads
 export
     bcd,
     get_callback,
@@ -68,22 +68,23 @@ function suff_stat_Tb(y::Matrix{SVector{3,T}}, Q_inv, u, Ns, Tb_out) where {T}
 end
 
 function nlogp(y::Matrix{SVector{3,type}}, u, T, b, Q_inv, Ns) where {type}
-    C = zero(type)
+    C = Atomic{type}(0.0)
     N = size(y,2)
     N_triads = size(y,1)
     N_orientions = length(Ns)
-    for n = 1:N
+    @threads for n = 1:N
         for k = 1:N_triads
             e = T[k]*u[k,n] + b[k] - y[k,n]
             c_k_n = dot(e, Q_inv[k], e)
             if n <= N_orientions
                 c_k_n *= Ns[n]
             end
-            C += c_k_n
+            # C += c_k_n
+            atomic_add!(C,c_k_n)
         end
     end
     # Average log likelihood
-    return C/prod(size(y))
+    return C[]/prod(size(y))
 end
 function nlogp_naive(y::Matrix{SVector{3,type}}, Q_inv, Ns, r, T, b, g, w, w_dot, s) where {type}
     C = zero(type)
@@ -261,7 +262,7 @@ function inverse_affine(y, T, b)
     return u
 end
 function inverse_affine!(u, y, T, b)
-    for n = 1:size(y,2)
+    @threads for n = 1:size(y,2)
         for k = 1:size(y,1)
             u[k,n] = T[k]\(y[k,n] - b[k])
         end
@@ -436,6 +437,50 @@ function dynamics!!(w::Vector{SVector{3,T}}, w_dot::Vector{SVector{3,T}}, s::Vec
         w[n] = gauss_newton(w[n], e, u[n], P, r, Ng, g_tol, i_max, y_pred, J_m)
         h!(y_pred, w[n], r, Ng)
         e = u[n] - SVector(y_pred)
+        phi = WLS*e
+        w_dot[n] = phi[inds_w_dot]
+        s[n] = phi[inds_s]
+    end
+    w, w_dot, s
+end
+function dynamics_threads!!(w::Vector{SVector{3,T}}, w_dot::Vector{SVector{3,T}},
+    s::Vector{SVector{3,T}},
+    u::Vector{SVector{N,T}}, Qu_inv::SMatrix{N,N,T},
+    r, Ng, g_tol, i_max) where{N,T}
+    N_dynamics = length(w)
+
+    H_m = zero(MMatrix{N, 6, T})
+    Na = size(r, 2)
+    fill_H_constants!(H_m, Na)
+    H!(H_m, r)
+    H = SMatrix(H_m)
+
+    y_pred = [zero(MVector{N, T}) for _ = 1:nthreads()]
+
+    inds_w_dot = @SVector [1,2,3]
+    inds_s = @SVector [4,5,6]
+
+    # Buffs
+    J_m = map(1:nthreads()) do n
+        J = zero(MMatrix{N, 3, T})
+        fill_J_h_buff!(J, Na, Ng)
+        J
+    end
+
+    H_T_Q_inv = H'*Qu_inv
+    WLS = (H_T_Q_inv*H)\H_T_Q_inv
+    P = Qu_inv - H_T_Q_inv'*WLS
+
+
+    @threads for n = 1:N_dynamics
+        e = zero(SVector{N, T})
+        phi = zero(SVector{6, T})
+        y_pred_i = y_pred[threadid()]
+        J_m_i = J_m[threadid()]
+
+        w[n] = gauss_newton(w[n], e, u[n], P, r, Ng, g_tol, i_max, y_pred_i, J_m_i)
+        h!(y_pred_i, w[n], r, Ng)
+        e = u[n] - SVector(y_pred_i)
         phi = WLS*e
         w_dot[n] = phi[inds_w_dot]
         s[n] = phi[inds_s]
@@ -698,5 +743,178 @@ function get_callback_maxdiff_param(r_true, T_true, b_true, show_every = 1)
     return callback
 
 end
+function bcd2!(r,T,b, y, Q_inv, Ns, g_mag::TT, ::Val{N_sens},
+    ::Val{Na},
+    tol_interval, i_max_g, tol_gs, i_max_w, tol_bcd, i_max_bcd;
+    callback = nothing) where {TT, N_sens, Na}
+
+    @assert istriu(T[1])
+    @assert r[1] == zeros(3)
+
+    N = size(y, 2)
+    N_triads = size(y, 1)
+    Ng = N_triads - Na
+    N_orientions = length(Ns)
+    N_dynamics = N - N_orientions
+
+    r_m = zero(MMatrix{3,Na,TT})
+    for k = 2:Na
+        r_m[:,k] = r[k]
+    end
+    u = [zero(SVector{3,TT}) for i = 1:N_triads, j = 1:N]
+    Qu_inv = [zero(SMatrix{3,3,TT}) for i = 1:N_triads]
+
+    # Statics
+    g = [zero(SVector{3,TT}) for _ = 1:N_orientions]
+
+    # Dynamics
+    u_m = zero(MVector{N_sens,TT})
+    u_n = [zero(SVector{N_sens,TT}) for _ = 1:N_dynamics]
+    Qu_inv_tall = zero(MMatrix{N_sens, N_sens, TT, N_sens^2})
+    # Initial w0 from gyroscopes
+    w = map(1:N_dynamics) do n
+        mapreduce(k -> y[Na + k, n + N_orientions], +, 1:Ng)/Ng
+    end
+
+    w_dot = [zero(SVector{3,TT}) for _ = 1:N_dynamics]
+    s = [zero(SVector{3,TT}) for _ = 1:N_dynamics]
+    Jrs = [zero(SMatrix{3,3,TT}) for _ = 1:N_dynamics]
+    Tg = zero(MMatrix{3,3,TT})
+
+    ii_T_1 = SVector{6,Int}([1,2,3,4,5,6])
+    ii_b_1 = SVector{3,Int}([7,8,9])
+
+    ii_T = SVector{9,Int}([1,2,3,4,5,6,7,8,9])
+    ii_b = SVector{3,Int}([10,11,12])
+
+
+    k = 0
+    log_p_prev::TT = Inf
+    dlogp = zero(TT)
+    logp = zero(TT)
+    converged = false
+    while !converged
+        k += 1
+
+        inverse_affine!(u, y, T, b)
+        inverse_affine_Q!(Qu_inv, Q_inv, T)
+
+        gravity!(g, u, Qu_inv, Na, N_orientions, g_mag, tol_interval, i_max_g)
+
+        for k = 1:N_triads
+            kk = 1+3*(k-1):3*k
+            Qu_inv_tall[kk,kk] = Qu_inv[k]
+        end
+        for n = 1:N_dynamics
+            for k = 1:N_triads
+                u_m[1+3(k-1):3*k] = u[k,n+N_orientions]
+            end
+            u_n[n] = SVector(u_m)
+        end
+
+        dynamics_threads!!(w, w_dot, s, u_n, SMatrix(Qu_inv_tall), SMatrix(r_m), Ng,
+        tol_gs, i_max_w)
+
+        # Positions
+        @threads for n = 1:N_dynamics
+            Jrs[n] = J_r(w[n], w_dot[n])
+        end
+        @threads for k = 2:Na
+            A_r, a_r = zero(SMatrix{3,3, TT}), zero(SVector{3, TT})
+            for n = 1:N_dynamics
+                J = Jrs[n]'*Qu_inv[k]
+                A_r += J*Jrs[n]
+                a_r += J*(u[k,n + N_orientions] - s[n])
+            end
+            r[k] = A_r\a_r
+            r_m[:,k] = r[k]
+        end
+
+        # Update u
+        @threads for n = 1:N
+            for k = 1:N_triads
+                # Static
+                if n <= N_orientions
+                    if k <= Na
+                        u[k,n] = -g[n]
+                    else
+                        u[k,n] = zero(SVector{3, TT})
+                    end
+                else
+                    n_d = n - N_orientions
+                    if k <= Na
+                        u[k,n] = Jrs[n_d]*r[k] + s[n_d]
+                    else
+                        u[k,n] = w[n_d]
+                    end
+                end
+            end
+        end
+
+        # Ref T
+        A_Tb_1, a_Tb_1 = zero(SMatrix{9,9, TT}), zero(SVector{9, TT})
+        for n = 1:N
+            A_1 = J_Tb_ref(u[1,n])
+            A_T_Q_inv_1 = A_1'*Q_inv[1]
+            if n <= N_orientions
+                A_T_Q_inv_1 *= Ns[n]
+            end
+            A_Tb_1 += A_T_Q_inv_1*A_1
+            a_Tb_1 += A_T_Q_inv_1*y[1,n]
+        end
+        x_1 = A_Tb_1\a_Tb_1
+        T[1] = BcdV3.T_ref(x_1[ii_T_1])
+        b[1] = x_1[ii_b_1]
+
+        # T and b estimation
+        @threads for k = 2:size(y,1)
+            A_Tb, a_Tb = zero(SMatrix{12,12, TT}), zero(SVector{12, TT})
+            for n = 1:size(y,2)
+                A = J_Tb(u[k,n])
+                A_T_Q_inv = A'*Q_inv[k]
+                if n <= N_orientions
+                    A_T_Q_inv *= Ns[n]
+                end
+                A_Tb += A_T_Q_inv*A
+                a_Tb += A_T_Q_inv*y[k,n]
+            end
+            x = A_Tb\a_Tb
+            T[k] = SMatrix{3,3, TT}(reshape(x[ii_T], 3,3))
+            b[k] = x[ii_b]
+        end
+
+        # Normalize Tg
+        C::TT = 0.0
+        for k = 1:Ng
+            C += tr(T[k+Na])
+        end
+        C /= (3*Ng)
+        for k = 1:Ng
+            Tg .= MMatrix(T[k+Na])
+            for i = 1:3
+                Tg[i,i] /= C
+            end
+            T[k+Na] = SMatrix(Tg)
+        end
+
+        logp = nlogp(y, u, T, b, Q_inv, Ns)
+        dlogp = log_p_prev - logp
+        log_p_prev = logp
+
+        if callback != nothing
+            callback(k, dlogp, logp, r, T, b)
+        end
+
+        converged = k == i_max_bcd || abs(dlogp) < tol_bcd
+    end
+    if k == i_max_bcd
+        @warn @sprintf(
+            "BCD: Max iterations %d reached. Criterion |dlopp| = %.2e < %.1e = tol \n",
+            k, abs(dlogp), tol_bcd
+        )
+    end
+    return r, T, b
+end
+
 
 end # module
